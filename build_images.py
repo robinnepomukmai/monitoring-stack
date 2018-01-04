9fec62283f09607843fddf5ee37014d681a90f71
Robin Mai <rmai@cmdea046142.de.obi.net>
2018-01-04 15:15:37 +0100

    add arme leute versionierung

--- build_images.py
+++ build_images.py
@@ -3,13 +3,15 @@ import docker
 client = docker.from_env()
 
 DOCKER_REGISTRY = "localhost:5000"
+MONITORING_STACK_VERSION = "1"
 
 containersToBuild = ["prometheus", "grafana", "alertmanager"]
 
 for container in containersToBuild:
-    print("building docker imager for: " + container)
-    client.images.build(path=container, tag=DOCKER_REGISTRY + "/" + "obi/" + container + ":1.0")
+    repositoryName = "obi/" + container
+    print("building docker imager for: " + repositoryName)
+    client.images.build(path=container, tag=DOCKER_REGISTRY + "/" + repositoryName + MONITORING_STACK_VERSION)
     print("Push Image to Docker Registry")
 
-    for line in client.images.push(DOCKER_REGISTRY + "/" + "obi/" + container, stream=True):
+    for line in client.images.push(DOCKER_REGISTRY + "/" + repositoryName, stream=True):
       print(line)
--- start_monitoring_stack.py
+++ start_monitoring_stack.py
@@ -3,10 +3,13 @@ import docker
 client = docker.from_env()
 
 DOCKER_REGISTRY = "localhost:5000"
+MONITORING_STACK_VERSION = "1"
+
 
 containersToStart = ["prometheus", "grafana", "alertmanager"]
 
 for service in containersToStart:
+    repositoryName = "obi/" + service
     print("starting... " + service)
-    lol = client.containers.run(DOCKER_REGISTRY + "/" + "obi/" + service + ":1.0", detach=True)
-    print(lol)
+    dockerOutput = client.containers.run(DOCKER_REGISTRY + "/" + repositoryName + MONITORING_STACK_VERSION, detach=True)
+    print(dockerOutput)
