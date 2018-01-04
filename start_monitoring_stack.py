91eed4f8579802d8cda6d44c85bfa4c57bb566eb
Robin Mai <rmai@cmdea046142.de.obi.net>
2018-01-04 16:45:11 +0100

    add prometheus as grafana datasource

--- start_monitoring_stack.py
+++ start_monitoring_stack.py
@@ -1,9 +1,10 @@
 #!/usr/local/bin/python3
-import docker
+import docker, requests, os, json
 client = docker.from_env()
 
 DOCKER_REGISTRY = "localhost:5000"
 MONITORING_STACK_VERSION = "1"
+GRAFANA_URL = 'http://localhost:3000'
 
 
 containersToStart = ["prometheus", "grafana", "alertmanager"]
@@ -20,3 +21,8 @@ for service in containersToStart:
     print("starting... " + service)
     dockerOutput = client.containers.run(DOCKER_REGISTRY + "/" + repositoryName + ":" + MONITORING_STACK_VERSION, detach=True, ports=ports, remove=True)
     print(dockerOutput)
+
+
+os.system("curl --user admin:admin 'http://localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{\"name\":\"prometheus\",\"isDefault\":true ,\"type\":\"prometheus\",\"url\":\"http://localhost:9090\",\"access\":\"proxy\",\"basicAuth\":false}'")
+
+print("\n\nFINISHED. Have fun :)")
