f87cb698f158d62a29af1146b6fd8e5254b084b7
Robin Mai <rmai@cmdea046142.de.obi.net>
2018-01-04 13:36:49 +0100

    remove bash file

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t lucify-grafana
-docker build -t lucify-prometheus
-docker build -t lucify-prometheus
-
-docker run -d -p 3000:3000 -e "GF_SECURITY_ADMIN_PASSWORD=admin_password" -v ~/grafana_db:/var/lib/grafana grafana/grafana
